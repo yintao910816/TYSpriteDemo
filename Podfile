8314f8778e75301bb0652f25f255b1bfad07a85a
slsyswift <mlsy.swift@gmail.com>
2025-01-03 11:07:13 +0700

    init

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+
+target 'TYSpriteDemo' do
+
+  use_frameworks!
+
+  pod 'TYKit', :path => './TYKit'
+
+end
---
+++ TYSpriteDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TYSpriteDemo
+//
+//  Created by apple on 2025/1/2.
+//
+
+import UIKit
+import TYKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        TestC.printTest()
+        
+        return true
+    }
+
+
+}
+
